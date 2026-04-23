05d76d4a9403f4213f4770c81a298de0942cd100
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-04-24 06:34:45 +0700

    feat(backend): add file_share entity

---
+++ backend/src/entity/file_share.rs
@@ -0,0 +1,33 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "file_shares")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: Uuid,
+    pub file_id: Uuid,
+    #[serde(skip_serializing)]
+    pub token_hash: String,
+    pub expires_at: Option<ChronoDateTimeUtc>,
+    pub created_at: ChronoDateTimeUtc,
+    pub revoked_at: Option<ChronoDateTimeUtc>,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::file_object::Entity",
+        from = "Column::FileId",
+        to = "super::file_object::Column::Id"
+    )]
+    File,
+}
+
+impl Related<super::file_object::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::File.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
--- backend/src/entity/mod.rs
+++ backend/src/entity/mod.rs
@@ -1,5 +1,6 @@
 pub mod api_token;
 pub mod email_verification;
 pub mod file_object;
+pub mod file_share;
 pub mod password_reset;
 pub mod user;
